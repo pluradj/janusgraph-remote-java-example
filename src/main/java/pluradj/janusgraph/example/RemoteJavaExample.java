ac5aac1c863e55d7cd5e43ebe0e747333fbc5a7b
Jason Plurad <pluradj@us.ibm.com>
2018-11-26 15:59:26 -0500

    Updated example

--- src/main/java/pluradj/janusgraph/example/RemoteJavaExample.java
+++ src/main/java/pluradj/janusgraph/example/RemoteJavaExample.java
@@ -6,9 +6,11 @@ import org.apache.tinkerpop.gremlin.driver.Cluster;
 import org.apache.tinkerpop.gremlin.driver.MessageSerializer;
 import org.apache.tinkerpop.gremlin.driver.ResultSet;
 import org.apache.tinkerpop.gremlin.driver.ser.GryoMessageSerializerV3d0;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.structure.io.gryo.GryoMapper;
 import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph;
 import org.janusgraph.graphdb.tinkerpop.JanusGraphIoRegistry;
@@ -33,11 +35,14 @@ public class RemoteJavaExample {
         Client client = cluster.connect().init();
 
         // demonstrate Java driver passing Gremlin as a string to be executed on the remote server
-        LOGGER.info("create a new edge");
+        ResultSet results = null;
         String gremlin = "g.addV('a').as('a').addV('b').addE('to').from('a').next()";
-        ResultSet results = client.submit(gremlin);
-        Edge edge = results.one().getEdge();
-        LOGGER.info(edge.toString());
+        for (int i=0; i<10; i++) {
+            LOGGER.info("create a new edge");
+            results = client.submit(gremlin);
+            Edge edge = results.one().getEdge();
+            LOGGER.info(edge.toString());
+        }
 
         // demonstrate Java GLV using Gremlin natively in app and executed on the remote server
         Graph graph = EmptyGraph.instance();
@@ -47,6 +52,10 @@ public class RemoteJavaExample {
             list.forEach((v) -> LOGGER.info(v.toString()));
             list = g.E().valueMap(true).toList();
             list.forEach((e) -> LOGGER.info(e.toString()));
+            // https://stackoverflow.com/questions/53464400/pagination-in-janusgraph-how-do-i-implement-traversal-nextpagesize-in-java
+            GraphTraversal<Vertex, Vertex> t = g.V().hasLabel("b");
+            List<Vertex> vl = t.next(5);
+            vl.forEach((v) -> LOGGER.info(v.toString()));
         } catch (Exception e) {
             LOGGER.error(e.getMessage(), e);
         }
